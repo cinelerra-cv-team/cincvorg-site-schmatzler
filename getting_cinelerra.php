ae7d0a1ebcd9465573d1e0e70b688983ead2d796
raffa <raffaella.traniello@livecom.it>
2009-10-04 16:28:47 +0200

    Add AVLinux as liveCD with CinelerraCV

--- getting_cinelerra.php
+++ getting_cinelerra.php
@@ -24,17 +24,18 @@ through the use of Live CDs. Here are some of the Live CD's known to
 contain cinelerra</p>
 
 <ul>
-<li><a href='http://www.knoppix.org'>Knoppix</a> - the "original" Debian-based LiveDistro</li>
+<li><a href='http://www.bandshed.net/AVLinux.html>AVLinux</a> - based on Debian, with LXDE by default and an optional XFCE</li>
+<li><a href='http://artistx.org'>ArtistX</a> - based on Ubuntu Intrepid, Gnome or KDE,
+for multimedia</li>
 <li><a href='http://www.dynebolic.org'>dyne:bolic</a> -  for multimedia production -uses Window Maker window manager</li>
 <li><a href='http://www.elivecd.org/'>Elive</a> -  debian based live CD using Enlightenment window manager</li>
 <li><a href='http://http://x-evian.org'>x-evian</a> - Debian based live CD for multimedia creations</li>
 <li><a href='http://www.mediainlinux.org'>MediainLinux</a> - Knoppix based live CD</li>
 <li><a href='http://musix.org.ar'>Musix</a> - Knoppix based - for audio production, graphic design and video editing</li>
-<li><a href='http://garbure.org/pho/'>pho (garbure)</a> - dedicated to video editing</li>
-<li><a href='http://linux.slo-tech.com'>Slo-Tech</a> - based on Debian and Morphix</li>
-<li><a href='http://artistx.org'>ArtistX</a> - based on Debian, Gnome or KDE,
-for multimedia</li>
 <li><a href='http://dl.getdropbox.com/u/331020/Mindmap/index.html'>openArtist</a> - based on Ubuntu Hardy, for artists</li>
+<li><a href='http://linux.slo-tech.com'>Slo-Tech</a> - based on Debian and Morphix</li>
+<li><a href='http://garbure.org/pho/'>pho (garbure)</a> - dedicated to video editing</li>
+<li><a href='http://www.knoppix.org'>Knoppix</a> - the "original" Debian-based LiveDistro</li>
 </ul>
 
 
@@ -397,7 +398,7 @@ git clone git://git.cinelerra.org/j6t/cinelerra.git my_cinelerra
 
  </div>
  
-<address> Last modified on Feb 11 2009 </address>
+<address> Last modified on Oct 4 2009 </address>
 
 <?php include "footer.php"; ?>
  </body>
