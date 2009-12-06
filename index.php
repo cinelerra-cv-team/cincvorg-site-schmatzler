fd0f48f416898c2a3b2b903425ab6e0b5b344706
raffa <raffaella.traniello@livecom.it>
2009-12-06 21:58:17 +0100

    News about the new bugtracker

--- index.php
+++ index.php
@@ -13,101 +13,60 @@ include "newsidemenu.php";
 <div class='content'>
 
 <h2><a name="news">News</a></h2>
+
+<p>
+<strong>Dec &nbsp;6, 2009</strong><br>
+* The migration of our bugtracker from skolelinux.no to cinelerra.org is completed, thanks to the precious help of Cillian De Roiste. We moved also from Bugzilla to Trac. 
+The bugtracker address is still <a href="http://bugs.cinelerra.org/">http://bugs.cinelerra.org/</a>; log-in details are unchanged.
+</p>
+
 <p>
 <strong>Sept &nbsp;24, 2009</strong><br>
 * <a href="http://heroinewarrior.com/index.php">Heroine Virtual LTD</a> releases <a href="http://heroinewarrior.com/cinelerra.php#download">Cinelerra 4.1</a>, featuring nested sequences. 64 bit binaries for Fedora 11 and for Ubuntu 9.04 are also available.</a>
 </p>
+
 <p>
 <strong>June &nbsp;1, 2009</strong><br>
 * CinelerraCV will be at the <a href="http://openvideoconference.org/">Openvideo Conference</a> in New York on 19-20 June 2009. Scott Frase will represent the community.
 </p>
+
 <p>
 <strong>May &nbsp;8, 2009</strong><br>
 * Leo Laporte and Randal Schwartz talk about Cinelerra and Lumiera on <a href="http://odtv.me/2009/05/06/floss-68/">FLOSS Weekly podcast #68</a> on <a href="http://twit.tv/FLOSS">Twit.tv</a>. Guests: Joel Holdsworth and Aaron Newcomb.
 </p>
+
 <p>
 <strong>January &nbsp;21, 2009</strong><br>
 * Cinelerra-cv now supports dnxhd codec. See git <a href="http://git.cinelerra.org/gitweb?p=j6t/cinelerra.git;a=commitdiff;h=954a91e72bba1ed1ce641b5faee7d4c5f318c074">commit</a>.
 </p>
+
 <p>
 <strong>December &nbsp;31, 2008</strong><br>
 * The new Lumiera logo has been chosen. Admire it <a href="http://lumiera.org/credits.html">here</a>.
 </p>
+
 <p>
 <strong>October &nbsp;26, 2008</strong><br>
 * <a href="http://lumiera.org">Lumiera</a> is looking for a logo. Enter the <a href="http://www.pipapo.org/pipawiki/Lumiera/Logos">Logo Contest</a>!.
 </p>
+
 <p>
 <strong>September &nbsp;26, 2008</strong><br>
 * We say farewell to the SVN repo that served us so well. The Cinelerra CV code is now maintained in <a href="http://git.or.cz">git</a>.
 </p>
+
 <p>
 <strong>September &nbsp;23, 2008</strong><br>
 * Roland "wildhostile" set up a <a href="http://makefx.wordpress.com">Cinelerra blog</a>. Write an article or a tutorial, add a video or localize yourself on the Cinelerra users' map!  
 </p>
-<p>
-<strong>August &nbsp;30, 2008</strong><br>
-* <a href="http://akiradproject.net/wellcome">akirad repository</a> offers intrepid Cinelerra-CV users a <a href="getting_cinelerra.php#intrepid">package</a> to test on 
-Ubuntu 8.10 Intrepid Ibex.  
-</p>
-<p>
-<strong>August&nbsp;11, 2008</strong><br>
-* <a href="http://www.heroinewarrior.com/index.php3">Heroine Virtual LTD</a> releases <a href="http://www.heroinewarrior.com/cinelerra.php3">Cinelerra 4</a>. Have a look at the official website for a detailed list of <a href="http://www.heroinewarrior.com/cinelerra_changelog.4.txt">changes</a> and for Ubuntu binary packages.
-</p>
 
-<p>
-<strong>August&nbsp;8, 2008</strong><br>
-* Cinelerra.org wears an improved CSS.
-</p>
-<p>
-<strong>July &nbsp;19, 2008</strong><br>
-* A <a href="http://www.vimeo.com/groups/cinelerra">cinelerra group</a> has been created on <a href="http://www.vimeo.com">Vimeo</a> by Leandro Ribeiro for both <a href="http://www.heroinewarrior.com/cinelerra.php3">HV</a> and <a href="http://www.cinelerra.org">CV</a> version users. Share your videos!
-</p>
-
-<p>
-<strong>June &nbsp;29, 2008</strong><br>
-* The <a href="http://akiradproject.net/wellcome">akirad repository</a> where Paolo Rampino makes Cinelerra packages available for Ubuntu, is mirrored at <a href="http://akirad.cinelerra.org/">akirad.cinelerra.org</a>.
-</p>
-
-<p>
-<strong>April &nbsp;20, 2008</strong><br>
-* CinelerraCV is moving. Web pages and DokuWiki have moved to a new server, in Germany (IP number 78.47.64.243). For details see the
- <a href="migration.php">migration status page</a>.   
-</p>
-
-<p>
-<strong>April &nbsp;4, 2008</strong><br>
-* <a href="http://lumiera.org/">Lumiera</a>, the new video editor developed by the CinelerraCV community, has
- started building an independent website.
-</p>
-
-<p>
-<strong>March &nbsp;6, 2008</strong><br>
-* 'Cinelerra3' is now an independent project called Lumiera. The name was chosen by
- community vote on the best names proposed.
-</p>
-
-<p>
-<strong>March &nbsp;5, 2008</strong><br>
-* CinelerraCV <a href="getting_cinelerra.php#hardy">packages</a> are already available for Ubuntu 8.04 Hardy Heron.  
-</p>
-
-<p>
-<strong>February &nbsp;14, 2008</strong><br>
-* Cinelerra interface is now available also in Russian.
-</p>
-
-<p>
-<strong>February &nbsp;1, 2008</strong><br>
-* First official IRC meeting for the developers of the 'Cinelerra3' project.
-</p>
 
 <hr>
 
 <p><a href="oldnews.php">Older news...</a></p>
 </div>
 
-<address>Last modified on 7 June 2009</address>
+<address>Last modified on 6 December 2009</address>
 
 <?php include "footer.php"; ?>
 </body>
--- oldnews.php
+++ oldnews.php
@@ -21,6 +21,65 @@ These are the older ones, back to the beginning in April 2003.
 <div class"body">
 <div class="content">
 <hr>
+
+<p>
+<strong>August &nbsp;30, 2008</strong><br>
+* <a href="http://akiradproject.net/wellcome">akirad repository</a> offers intrepid Cinelerra-CV users a <a href="getting_cinelerra.php#intrepid">package</a> to test on 
+Ubuntu 8.10 Intrepid Ibex.  
+</p>
+
+<p>
+<strong>August&nbsp;11, 2008</strong><br>
+* <a href="http://www.heroinewarrior.com/index.php3">Heroine Virtual LTD</a> releases <a href="http://www.heroinewarrior.com/cinelerra.php3">Cinelerra 4</a>. Have a look at the official website for a detailed list of <a href="http://www.heroinewarrior.com/cinelerra_changelog.4.txt">changes</a> and for Ubuntu binary packages.
+</p>
+
+<p>
+<strong>August&nbsp;8, 2008</strong><br>
+* Cinelerra.org wears an improved CSS.
+</p>
+
+<p>
+<strong>July &nbsp;19, 2008</strong><br>
+* A <a href="http://www.vimeo.com/groups/cinelerra">cinelerra group</a> has been created on <a href="http://www.vimeo.com">Vimeo</a> by Leandro Ribeiro for both <a href="http://www.heroinewarrior.com/cinelerra.php3">HV</a> and <a href="http://www.cinelerra.org">CV</a> version users. Share your videos!
+</p>
+
+<p>
+<strong>June &nbsp;29, 2008</strong><br>
+* The <a href="http://akiradproject.net/wellcome">akirad repository</a> where Paolo Rampino makes Cinelerra packages available for Ubuntu, is mirrored at <a href="http://akirad.cinelerra.org/">akirad.cinelerra.org</a>.
+</p>
+
+<p>
+<strong>April &nbsp;20, 2008</strong><br>
+* CinelerraCV is moving. Web pages and DokuWiki have moved to a new server, in Germany (IP number 78.47.64.243). For details see the
+ <a href="migration.php">migration status page</a>.   
+</p>
+
+<p>
+<strong>April &nbsp;4, 2008</strong><br>
+* <a href="http://lumiera.org/">Lumiera</a>, the new video editor developed by the CinelerraCV community, has started building an independent website.
+</p>
+
+<p>
+<strong>March &nbsp;6, 2008</strong><br>
+* 'Cinelerra3' is now an independent project called Lumiera. The name was chosen by
+ community vote on the best names proposed.
+</p>
+
+<p>
+<strong>March &nbsp;5, 2008</strong><br>
+* CinelerraCV <a href="getting_cinelerra.php#hardy">packages</a> are already available for Ubuntu 8.04 Hardy Heron.  
+</p>
+
+<p>
+<strong>February &nbsp;14, 2008</strong><br>
+* Cinelerra interface is now available also in Russian.
+</p>
+
+<p>
+<strong>February &nbsp;1, 2008</strong><br>
+* First official IRC meeting for the developers of the 'Cinelerra3' project.
+</p>
+
 <p>
 <strong>December &nbsp;31, 2007</strong><br>
 * The Cinelerra CV manual is now available in <a
