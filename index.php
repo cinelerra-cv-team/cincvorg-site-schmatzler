c780327472f0e076a81d3f6fb1284b575e247eed
Raffaella Traniello <raffa@raffa-desktop.(none)>
2010-10-26 00:22:44 +0200

    Added OpenvideoConference and CinHV4.2 news

--- index.php
+++ index.php
@@ -15,63 +15,34 @@ include "newsidemenu.php";
 <h2><a name="news">News</a></h2>
 
 <p>
-<strong>April &nbsp;19, 2010</strong><br>
+<strong>Oct 15, 2010</strong><br>
+* While we are working on CinelerraCV 2.1.5, <a href="http://heroinewarrior.com/index.php">Heroine Virtual LTD</a> releases <a href="http://heroinewarrior.com/cinelerra.php#download">Cinelerra 4.2</a>. Of main interest might be the "Edit->Align edits" feature, which aligns all the audio edits with the video.
+Then there's the keyframe spanning feature, where highlighting a region with keyframe generation on causes effect tweeks to span all the keyframes.
+Then there's the fact that all assets are opened in subprocesses so they don't bring down the entire program when they crash. featuring nested sequences. Only the source code is available, no packages.</a>
+</p>
+
+<p>
+<strong>October 1-2, 2010</strong><br>
+* The CinelerraCV community is at the <a href="http://openvideoconference.org/">Openvideo Conference</a> in New York.
+</p>
+
+<p>
+<strong>April 19, 2010</strong><br>
 * CinelerraCV will be at the <a href="http://ctfilmfest.com/">Connecticut Film Festival</a> - Danbury CT on May 7 2010. The festival will pay a special attention to Free Software with dedicate talks and workshops. Scott Frase will represent the community doing a <a href="http://ctfilmfest.com/site/node/43">presentation on Video production using Free Software tools</a>.
 </p>
 
 <p>
-<strong>Dec &nbsp;6, 2009</strong><br>
+<strong>Dec 6, 2009</strong><br>
 * The migration of our bugtracker from skolelinux.no to cinelerra.org is completed, thanks to the precious help of Cillian De Roiste. We moved also from Bugzilla to Trac. 
 The bugtracker address is still <a href="http://bugs.cinelerra.org/">http://bugs.cinelerra.org/</a>; log-in details are unchanged.
 </p>
-
-<p>
-<strong>Sept &nbsp;24, 2009</strong><br>
-* <a href="http://heroinewarrior.com/index.php">Heroine Virtual LTD</a> releases <a href="http://heroinewarrior.com/cinelerra.php#download">Cinelerra 4.1</a>, featuring nested sequences. 64 bit binaries for Fedora 11 and for Ubuntu 9.04 are also available.</a>
-</p>
-
-<p>
-<strong>June &nbsp;1, 2009</strong><br>
-* CinelerraCV will be at the <a href="http://openvideoconference.org/">Openvideo Conference</a> in New York on 19-20 June 2009. Scott Frase will represent the community.
-</p>
-
-<p>
-<strong>May &nbsp;8, 2009</strong><br>
-* Leo Laporte and Randal Schwartz talk about Cinelerra and Lumiera on <a href="http://odtv.me/2009/05/06/floss-68/">FLOSS Weekly podcast #68</a> on <a href="http://twit.tv/FLOSS">Twit.tv</a>. Guests: Joel Holdsworth and Aaron Newcomb.
-</p>
-
-<p>
-<strong>January &nbsp;21, 2009</strong><br>
-* Cinelerra-cv now supports dnxhd codec. See git <a href="http://git.cinelerra.org/gitweb?p=j6t/cinelerra.git;a=commitdiff;h=954a91e72bba1ed1ce641b5faee7d4c5f318c074">commit</a>.
-</p>
-
-<p>
-<strong>December &nbsp;31, 2008</strong><br>
-* The new Lumiera logo has been chosen. Admire it <a href="http://lumiera.org/credits.html">here</a>.
-</p>
-
-<p>
-<strong>October &nbsp;26, 2008</strong><br>
-* <a href="http://lumiera.org">Lumiera</a> is looking for a logo. Enter the <a href="http://www.pipapo.org/pipawiki/Lumiera/Logos">Logo Contest</a>!.
-</p>
-
-<p>
-<strong>September &nbsp;26, 2008</strong><br>
-* We say farewell to the SVN repo that served us so well. The Cinelerra CV code is now maintained in <a href="http://git.or.cz">git</a>.
-</p>
-
-<p>
-<strong>September &nbsp;23, 2008</strong><br>
-* Roland "wildhostile" set up a <a href="http://makefx.wordpress.com">Cinelerra blog</a>. Write an article or a tutorial, add a video or localize yourself on the Cinelerra users' map!  
-</p>
-
 
 <hr>
 
 <p><a href="oldnews.php">Older news...</a></p>
 </div>
 
-<address>Last modified on 19 Aprile 2010</address>
+<address>Last modified on 25 October 2010</address>
 
 <?php include "footer.php"; ?>
 </body>
--- oldnews.php
+++ oldnews.php
@@ -23,6 +23,46 @@ These are the older ones, back to the beginning in April 2003.
 <hr>
 
 <p>
+<strong>Oct 25, 2010</strong><br>
+* <a href="http://heroinewarrior.com/index.php">Heroine Virtual LTD</a> releases <a href="http://heroinewarrior.com/cinelerra.php#download">Cinelerra 4.1</a>, featuring nested sequences. 64 bit binaries for Fedora 11 and for Ubuntu 9.04 are also available.</a>
+</p>
+
+<p>
+<strong>June 1, 2009</strong><br>
+* CinelerraCV will be at the <a href="http://openvideoconference.org/">Openvideo Conference</a> in New York on 19-20 June 2009. Scott Frase will represent the community.
+</p>
+
+<p>
+<strong>May &nbsp;8, 2009</strong><br>
+* Leo Laporte and Randal Schwartz talk about Cinelerra and Lumiera on <a href="http://odtv.me/2009/05/06/floss-68/">FLOSS Weekly podcast #68</a> on <a href="http://twit.tv/FLOSS">Twit.tv</a>. Guests: Joel Holdsworth and Aaron Newcomb.
+</p>
+
+<p>
+<strong>January &nbsp;21, 2009</strong><br>
+* Cinelerra-cv now supports dnxhd codec. See git <a href="http://git.cinelerra.org/gitweb?p=j6t/cinelerra.git;a=commitdiff;h=954a91e72bba1ed1ce641b5faee7d4c5f318c074">commit</a>.
+</p>
+
+<p>
+<strong>December &nbsp;31, 2008</strong><br>
+* The new Lumiera logo has been chosen. Admire it <a href="http://lumiera.org/credits.html">here</a>.
+</p>
+
+<p>
+<strong>October &nbsp;26, 2008</strong><br>
+* <a href="http://lumiera.org">Lumiera</a> is looking for a logo. Enter the <a href="http://www.pipapo.org/pipawiki/Lumiera/Logos">Logo Contest</a>!.
+</p>
+
+<p>
+<strong>September &nbsp;26, 2008</strong><br>
+* We say farewell to the SVN repo that served us so well. The Cinelerra CV code is now maintained in <a href="http://git.or.cz">git</a>.
+</p>
+
+<p>
+<strong>September &nbsp;23, 2008</strong><br>
+* Roland "wildhostile" set up a <a href="http://makefx.wordpress.com">Cinelerra blog</a>. Write an article or a tutorial, add a video or localize yourself on the Cinelerra users' map!  
+</p>
+
+<p>
 <strong>August &nbsp;30, 2008</strong><br>
 * <a href="http://akiradproject.net/wellcome">akirad repository</a> offers intrepid Cinelerra-CV users a <a href="getting_cinelerra.php#intrepid">package</a> to test on 
 Ubuntu 8.10 Intrepid Ibex.  
