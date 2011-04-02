fbca00fbf24a47190dce506337b76a06c9d39ecc
Raffaella Traniello <raffa@raffa-desktop.(none)>
2011-04-02 12:12:17 +0200

    Moved old packages to a separate page, removed some dead repos.

--- getting_cinelerra.php
+++ getting_cinelerra.php
@@ -75,78 +75,15 @@ apt-get install cinelerra</code> .
 
   <h3><a name="apt">Debian</a></h3>
 
-<h4>
-<a href="#apt-x86">x86</a> |
-<a href="#apt-AMD64">Athlon64</a> |
-<a href="#apt-ppc">PowerPC</a> 
-</h4>
-
-<h4><a name="apt-x86">x86 (Intel, AMD, PC compatibles)</a></h4>
-
 <blockquote>
-<p>
-To install the package add one of the following lines to your sources list:<br><br>
-For i386 processors, by Valentina Messeri:<br>
-<code>deb http://giss.tv:8000/debian32 ./</code><br><br>
-For i386 processors, by Andraz Tori:<br>
-  <code>deb http://www.kiberpipa.org/~minmax/cinelerra/builds/sid/ ./</code><br><br>
-  For Pentium4 processors, by Andraz Tori:<br>
-  <code>deb http://www.kiberpipa.org/~minmax/cinelerra/builds/pentium4/ ./</code><br><br>
-  For Athlon processors, by Andraz Tori:<br>
-  <code>deb http://www.kiberpipa.org/~minmax/cinelerra/builds/athlonxp/ ./</code><br><br>
-   Apt-source, by Andraz Tori:<br>
-   <code>deb-src http://www.kiberpipa.org/~minmax/cinelerra/builds/sid/ ./</code><br><br>
-  You will need some additional packages not found in Debian's official
-  repositories, provided by Christian Marillat: <br>
-  <code>deb http://www.debian-multimedia.org/ sid main</code><br><br>
-  In order to use the mirror you need to add in your gpg keyring marillat's
-gpg-key <br> 
-  <code>gpg --keyserver hkp://wwwkeys.eu.pgp.net --recv-keys 1F41B907</code><br>
-  <code> gpg --armor --export 1F41B907 | sudo apt-key add - </code><br><br>
-  If you don't use sudo, do the following under root :<br>
-  <code>gpg --armor --export 1F41B907 | apt-key add -</code>
-</p>
+<p>Both CinelerraCV 2.1.0 and CinelerraHV 4.2 packages are available from <a href="http://debian-multimedia.org/">Debian-multimedia</a>.<br />
+Older packages are available too. See the <a href="old-distro.php">old packages</a> page.</p>
 </blockquote>
-
- <h4><a name="apt-AMD64"> For athlon64 processors</a></h4>
-
-<blockquote>
-AMD64 Debian packages are built by Valentina Messeri. <br>
-To install the package add this line to your sources list: <br>
-  <code>deb http://giss.tv/~vale/debian64 ./</code><br><br>
-Apt-source:<br>
-  <code>deb-src http://giss.tv/~vale/debian64 ./</code>
-
-</blockquote>
-
- <H4><a name="apt-ppc">PowerPC (e.g. Macintosh)</a></h4>
-
-<blockquote>
- <p>
-   PowerPC Debian packages are built from CVS by <a href="http://www.lamenagerie.com/">La Ménagerie</a>.<br>
-
-   You can add this line to your sources list:<br><br>
-   PowerPC:<br>
-   <code>deb http://garbure.org/debian/ ./</code><br><br>
-   PowerPC with AltiVec or Power4 optimisations:<br>
-   <i>Not yet</i><br>
-   Yes, we are searching people who can build cinelerra-cv on g4/g5. If you want
-to contribute but you cannot provide debian packages feel free to mail us/join
-irc channel. <br><br>
-   Apt-source:<br>
-   <code>deb-src http://garbure.org/debian/ ./</code><br><br>
-  You will need some additional packages not found in Debian's official
-   repositories, compiled from Christian Marillat sources repository for PowerPC:
-  <br>
-   <code>deb http://honk.physik.uni-konstanz.de/~agx/linux-ppc/debian/ mplayer/</code>
-  </p>
-</blockquote>
-
 
 <h3><a name="gentoo">Gentoo</a></h3>
 
 <blockquote>
-Installation for Gentoo GNU/Linux is very straight forward. Simply type: <BR>
+<p>Installation for Gentoo GNU/Linux is very straight forward. Simply type: <BR>
 <code>emerge cinelerra</code><BR>
 as root and it should install and run without any problems. <BR>
 Note that you may need to put cinelerra in your `/etc/portage/package.keywords'
@@ -155,28 +92,31 @@ file in order to unmask it:<BR>
 See the <a href='http://www.gentoo.org/doc/en/handbook/handbook-x86.xml?part=3&chap=3'>Handbook</a> 
 for details. <BR>
 More informations can be found in the <a
-href='http://cvs.cinelerra.org/docs/split_manual_en/cinelerra_cv_manual_en_2.html#SEC25'>Manual</a>.
+href='http://cvs.cinelerra.org/docs/split_manual_en/cinelerra_cv_manual_en_2.html#SEC25'>Manual</a>.</p>
 </blockquote>
 
 <h3><a name="fedora">Fedora</a></h3>
-<blockquote>
-Cinelerra is not available in the default Fedora repositories.<br>
+<p>
+<p>Cinelerra is not available in the default Fedora repositories.<br>
 Cinelerra is included in the <a
 href="http://freshrpms.net" >Freshrpms</a> repository but 
 the package collection of Freshrpms may conflict with other third-party repositories such as Livna.<br>
-Cinelerra is available also in the kwizart repository that, on the contrary, is compatible with Livna.<br></blockquote>
+Cinelerra is available also in the kwizart repository that, on the contrary, is compatible with Livna.<br></p>
+</blockquote>
+
 <h4><a name="fedora9">Fedora 9</a></h4>
+
 <blockquote>
-To install Cinelerra from kwizart repository (x86 and x86_64) do: <br>
+<p>To install Cinelerra from kwizart repository (x86 and x86_64) do: <br>
 <code>
 su -<br>
 rpm -ivh http://rpm.livna.org/livna-release-9.rpm<br>
 rpm -ivh http://rpms.kwizart.net/kwizart-release-9.rpm<br>
 yum install cinelerra --enablerepo=kwizart<br>
-</code>
+</code></p>
 </blockquote>
 <blockquote>
-Tips:<br>
+<p>Tips:<br>
 1 - This package tweaks the kernel.shmmax value in /etc/sysctl.conf so there is no need for you to modify it.<br>
 2 - This version points to the system fonts path in
 /usr/share/fonts (instead of /usr/lib(64)/cinelerra/fonts).<br>
@@ -189,29 +129,29 @@ to be seen by the title plugin, so for example:<br>
 <code>
 cd /usr/share/fonts/<subdir><br>
 ttmkfdir && mkfontdir
-</code>
+</code></p>
 </blockquote>
 
 <h4><a name="fedora8">Fedora 8</a></h4>
 
 <blockquote>
-To install Cinelerra from kwizart repository (x86 and x86_64) do:<br>
+<p>To install Cinelerra from kwizart repository (x86 and x86_64) do:<br>
 <code>
 su -<br>
 rpm -ivh http://rpms.kwizart.net/kwizart-release-8.rpm<br>
-yum install cinelerra --enablerepo=kwizart</code><br>
+yum install cinelerra --enablerepo=kwizart</code><br>.</p>
 </blockquote>
 <blockquote>
-Tips:<br>
+<p>Tips:<br>
 1 - This package tweaks the kernel.shmmax value in /etc/sysctl.conf so there is no need for you to modify it.<br>
 2 - The fonts tweak for Fedora 9 is not available for Fedora 8. You need
 to copy your fonts to /usr/lib(64)/cinelerra/fonts and regenerate
-fonts.dir.
+fonts.dir.</p>
 </blockquote>
 
 <h4><a name="fedora567">Fedora 5, 6 and 7</a></h4>
 <blockquote>
-The easiest way to
+<p>The easiest way to
 install packages from Freshrpms is to include the repository in the <B>yum</B>
 configuration.<BR>
 Also make sure that the Fedora Extras repository is enabled.  This is the case
@@ -221,17 +161,17 @@ On Fedora 6, as the user root do:<BR>
 On Fedora 5, do:<BR>
 <code>rpm -ivh http://ftp.freshrpms.net/pub/freshrpms/fedora/linux/5/freshrpms-release/freshrpms-release-1.1-1.fc.noarch.rpm</code><BR><BR>
 Then type:<BR>
-<code>yum -y install cinelerra</code><BR>
+<code>yum -y install cinelerra</code><BR></p>
 </blockquote>
 
 
 <h3><a name="mandriva">Mandriva</a></h3>
-
+<p>
 <blockquote>
 Cinelerra packages for Mandriva made by PLF are ready to install. See the <a
 href='http://plf.zarb.org/packages.php'>PLF</a> site for more information.
 </blockquote>
-
+</p>
 
 <h3><a name="slack">Slackware</a></h3>
 <blockquote>
@@ -265,60 +205,14 @@ RPMs for SuSE 9 are built from CVS by Kevin Brosius, and hosted at
 
 <h3><a name="ubuntu">Ubuntu</a></h3>
 
-<p>Cinelerra CV packages are available from three different repositories.</p>
-
-<h4>Launchpad</h4>
-<p><em>Latest Cinelerra CV 2.1.5 for 10.10 Maverick Meerkat, 10.04 Lucid Lynx, 9.10 Karmic Koala, 8.04 Hardy Heron, maintained by Nicola Ferralis</em></p>
+<blockquote>
+<p>Latest Cinelerra CV 2.1.5 for 10.10 Maverick Meerkat, 10.04 Lucid Lynx, 9.10 Karmic Koala, 8.04 Hardy Heron, are available from a Launchpad repo maintained by Nicola Ferralis</p>
 <blockquote>
 <code>https://launchpad.net/~cinelerra-ppa</code>
 </blockquote>
 <p>To add the Cinelerra PPA to your source list follow the instructions on the <a href="https://launchpad.net/~cinelerra-ppa/+archive/ppa">Launchpad page</a>.</p>
 
-<h4>Akirad</h4>
-<p><em>Cinelerra CV 2.1 for 10.04 Lucid Lynx, 9.10 Karmic Koala, 9.04 Jaunty Jackalope, 8.10 Intrepid Ibex, 
-for all x86, working on 32 and 64 bits, maintained by Paolo Rampino</em></p>
-<blockquote>
-<strong>(Dead repository)</strong>
-<code>deb http://akirad.cinelerra.org akirad-YOURVERSION main</code><br /><br />
-(Replace 'YOURVERSION' with 'lucid', 'karmic', 'jaunty','intrepid' or 'hardy', depending on your release)<br><br>
-<blockquote>
-Installation notes:<br>
-- For your convenience you can install a package for detecting your version of Ubuntu, installing akirad repository and keeping it updated.<br>
-Just double click on the link
-<a href="http://akirad.cinelerra.org/pool/addakirad.deb">http://akirad.cinelerra.org/pool/addakirad.deb</a> and install it with GDebi Package Installer.<br>
-Alternatively, use one of the following terminal commands:<br>
-        <code>wget -q http://akirad.cinelerra.org/pool/addakirad.deb && sudo dpkg -i addakirad.deb && rm addakirad.deb && sudo apt-get update</code><br>
-        or<br>
-<code>echo deb http://akirad.cinelerra.org akirad-YOURVERSION main | sudo tee /etc/apt/sources.list.d/akirad.list && wget -q http://akirad.cinelerra.org/dists/akirad.key -O- | sudo apt-key add - && sudo apt-get update</code><br>
-
-- 3 are the packages available in the akirad repository:<br>
-<blockquote>
-Comunity Version of Cinelerra<br>
-<blockquote>
-<i>cinelerra</i> (all computers)<br>
-<i>cinelerra-xt</i> (same of cinelerra, but with ssse3 enabled that gives about 20% more performance on recent intel cpus)<br>
-</blockquote>
-
-            Utility
-<blockquote>
-<i>cinelerra-swtc</i> (extra Shape Wipe Transitions)<br>
-</blockquote>
-</blockquote>
-- These packages set shmmax to 0x7fffffff and add non-English language support for Cinelerra.<br>
-- IN EVENT OF UPDATE PROBLEMS RUN THE FOLLOWING TERMINAL COMMAND:<br>
-<code>for i in sv cv cv-gl sv-gl cv-smp sv-smp; do sudo apt-get --force-yes --assume-yes remove cinelerra$i libquicktime$i libmpeg3$i libguicast$i; done && sudo apt-get install cinelerra</code><br>
-- Please, report any package bug to <em>akir4d at gmail dot com</em>
-</blockquote>
-</blockquote>
-
-<H4>giss.tv</a></h4>
-<p><em>Cinelerra CV for 8.04 Hardy Heron, for i386 (not working on amd 32 bits), maintained by by Valentina Messeri</em></p>
-<blockquote>
-<code>deb http://giss.tv/~vale/ubuntu32/hardy ./</code>
-</blockquote>
-
-<h4>Compilation</h4>
-You can find detailed instructions for compiling CinelerraCV from source on the <a href="http://www.g-raffa.eu/Cinelerra/HOWTO/compilation.html">Cinelerra for Grandma</a> tutorial.</p>
+<p> If you want to compile CinelerraCV from source you can find detailed instructions on the <a href="http://www.g-raffa.eu/Cinelerra/HOWTO/compilation.html">Cinelerra for Grandma</a> tutorial.</p>
 </blockquote>
 
 <h2><a name="source">Source code</a></h2>
@@ -332,7 +226,7 @@ git clone git://git.cinelerra.org/j6t/cinelerra.git my_cinelerra
 
  </div>
  
-<address> Last modified on 04 December 2010 </address>
+<address> Last modified on Apr 2, 2011 </address>
 
 <?php include "footer.php"; ?>
  </body>
