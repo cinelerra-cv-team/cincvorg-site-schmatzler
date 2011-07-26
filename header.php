30c33d9b21c10300529faae442ae6a74f5e145a1
Raffaella Traniello <raffa@raffa-desktop.(none)>
2011-07-26 18:37:59 +0200

    Add meta tag for charset

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
   <link rel="stylesheet" href="base.css" type="text/css">
-  <title>Cinelerra :: a video editor
-and compositor for Linux</title>
+  <title>Cinelerra :: a video editor and compositor for Linux</title>
 </head>
 <body>
 <?php
