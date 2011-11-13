66f6c86d412c5620687e9d6f14a9a59f083fffa4
Raffaella Traniello <raffa@raffa-desktop.(none)>
2011-11-13 21:28:31 +0100

    Add link to release announcement

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ Volunteers, please contact the <a href="https://init.linpro.no/mailman/skolelinu
 
 <p>
 <strong>Nov 13, 2011</strong><br>
-* <em>CinelerraCV 2.2</em> is released. It includes Hermann Vosseler's Bezier Patch (bezier automation for Cinelerra-CV fades, camera and projector), improved default settings, extended audio range, support for multiline label and clips comments, autodetection of OpenGL in configure, detection of v4l2.<br>
+* <em>CinelerraCV 2.2</em> is released. It includes Hermann Vosseler's Bezier Patch (bezier automation for Cinelerra-CV fades, camera and projector), improved default settings, extended audio range, support for multiline label and clips comments, autodetection of OpenGL in configure, detection of v4l2. For more details see the <a href="https://init.linpro.no/pipermail/skolelinux.no/cinelerra/2011-November/018309.html">release announcement</a>.<br>
 This release inaugurates the new <a href="http://git.cinelerra.org/gitweb?p=CinelerraCV.git;a=summary">community GIT repo</a> for Cinelerra. 
 </p>
 
