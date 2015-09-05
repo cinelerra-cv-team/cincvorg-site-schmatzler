e92ad299b3481e3e688085ab3fa9a2fda9535ca0
Danny Schmarsel <danny@schmarsel.de>
2015-09-05 10:53:52 +0200

    Merge mailing lists into contact information
    Double information we don't need.

--- contact.php
+++ contact.php
@@ -8,23 +8,29 @@ include "newsidemenu.php";
 ?>
 
 <div class="content">
-<h2>E-mail</h2>
- <ul>
-    <li>
-     The Cinelerra CV mailing list has moved from skolelinux.no! <br>
-     The new e-mail address is: 
-     &lt;cinelerra <strong>at</strong> lists.cinelerra-cv.org&gt;
-    </li>
-   </ul>
- <h2><a name="irc">Internet Relay Chat (IRC)</a></h2>
-   <p>We are on the channel <code>cinelerra</code> on <strong>Freenode</strong>.
-   If you don't know which server to connect to, try irc.freenode.net, port 6667.
-   <br>
-   The channel is logged into a <a href="http://irclog.cinelerra-cv.org/">
-   web archive</a>.
-   </p>
-  </div>
- <address> Last modified on March 31st 2014 </address>
+<h1 id="page-heading">How to get in touch with us</h1>
+
+<h2>E-Mail</h2>
+  <p>E-Mail is our main medium of communication.<p>
+  <p>Our mailing list brings both developers and users together. It is basically a forum, where users send mails to each other.</p>
+  <p>You can send an e-mail to <code>&lt;cinelerra <strong>at</strong> lists.cinelerra-cv.org&gt;</code> and all users subscribed to our mailing list will get it.</p>
+  <p>There is a web interface for <a href="http://lists.cinelerra-cv.org/cgi-bin/mailman/listinfo/cinelerra">subscribing</a> and a <a href="http://lists.cinelerra-cv.org/pipermail/cinelerra/">web archive</a> that anybody can browse.</p>
+  <p>You <u>don't</u> have to subscribe, but your e-mail will then be held waiting for moderator approval. The same goes for attachments bigger than 400KB.</p>
+  <p></p>
+  <p>We also have a <a href="https://lists.skolelinux.org/pipermail/cinelerra/">web archive of the old mailing list</a> which was mostly shut down in March 2014.</p>
+  <p>It is also available on <a href="http://www.mail-archive.com/cinelerra@skolelinux.no/">The Mail Archive</a></li> and <a href="http://dir.gmane.org/gmane.comp.video.cinelerra-cv.general">GMANE</a></p>
+  
+<h2>Internet Relay Chat (IRC)</h2>
+  <p>We are on the channel <code>#cinelerra</code> on <strong>Freenode</strong></p>.
+  <p>If you don't know which server to connect to, try irc.freenode.net, port 6667.</p>
+  <p>The channel is logged into a <a href="http://irclog.cinelerra-cv.org/">web archive</a>.</p>
+  
+<h2>Webmaster</h2>
+  <p>You've found a bug on the website or you have a suggestion for it? Please <a href="mailto:danny@schmarsel.de">send me a message!</a></p>
+  <p><b>Note:</b> If you'd like to make non-trivial changes or change extensive stuff, please contact the mailing list first for approval.</p>
+
+</div> 
+ 
 <?php include "footer.php"; ?>
  </body>
 </html>
