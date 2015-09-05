a893b61fa7d2d846d63a2d7eef54c3141f4865dc
Danny Schmarsel <danny@schmarsel.de>
2015-09-05 11:02:30 +0200

    Fix formatting on contact page

--- contact.php
+++ contact.php
@@ -15,13 +15,13 @@ include "newsidemenu.php";
   <p>Our mailing list brings both developers and users together. It is basically a forum, where users send mails to each other.</p>
   <p>You can send an e-mail to <code>&lt;cinelerra <strong>at</strong> lists.cinelerra-cv.org&gt;</code> and all users subscribed to our mailing list will get it.</p>
   <p>There is a web interface for <a href="http://lists.cinelerra-cv.org/cgi-bin/mailman/listinfo/cinelerra">subscribing</a> and a <a href="http://lists.cinelerra-cv.org/pipermail/cinelerra/">web archive</a> that anybody can browse.</p>
-  <p>You <u>don't</u> have to subscribe, but your e-mail will then be held waiting for moderator approval. The same goes for attachments bigger than 400KB.</p>
-  <p></p>
+  <p>You are <u>not forced</u> to subscribe, but your e-mail will then be held waiting for moderator approval. The same goes for attachments bigger than 400KB.</p>
+  <br />
   <p>We also have a <a href="https://lists.skolelinux.org/pipermail/cinelerra/">web archive of the old mailing list</a> which was mostly shut down in March 2014.</p>
   <p>It is also available on <a href="http://www.mail-archive.com/cinelerra@skolelinux.no/">The Mail Archive</a></li> and <a href="http://dir.gmane.org/gmane.comp.video.cinelerra-cv.general">GMANE</a></p>
   
 <h2>Internet Relay Chat (IRC)</h2>
-  <p>We are on the channel <code>#cinelerra</code> on <strong>Freenode</strong></p>.
+  <p>We are on the channel <code>#cinelerra</code> on <strong>Freenode</strong>.</p>
   <p>If you don't know which server to connect to, try irc.freenode.net, port 6667.</p>
   <p>The channel is logged into a <a href="http://irclog.cinelerra-cv.org/">web archive</a>.</p>
   
