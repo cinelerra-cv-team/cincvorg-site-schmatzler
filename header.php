39b14e90a1ba69089bc2dc2257420328d14fefd7
Danny Schmarsel <danny@schmarsel.de>
2015-09-05 07:14:34 +0200

    Add meta tag for Google webmastertools
    It's a pretty solid tool for seeing what we can optimize.

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+  <meta name="google-site-verification" content="CrWrb4banD-eOg3fmZxaRqW_6B8P5fCIxKsyDiSVjjg" />
   <link rel="stylesheet" href="base.css" type="text/css">
   <title>Cinelerra :: a video editor and compositor for Linux</title>
 </head>
