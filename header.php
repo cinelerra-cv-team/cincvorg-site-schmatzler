63f10670018b034316baafbd5bcf4cfe0b21ee9b
Danny Schmarsel <danny@schmarsel.de>
2015-09-07 06:28:40 +0200

    Fix-up OpenGraph

--- header.php
+++ header.php
@@ -3,8 +3,9 @@
 <head>
   <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
   <meta name="google-site-verification" content="CrWrb4banD-eOg3fmZxaRqW_6B8P5fCIxKsyDiSVjjg" />
-  <meta property="og:image" content="http://cinelerra-cv.org/cinelerra.png">
+  <meta property="og:image" content="http://cinelerra-cv.org/cinelerra.png" />
   <meta property="og:title" content="Cinelerra :: a video editor and compositor for Linux" />
+  <meta property="og:site_name" content="Cinelerra-CV.org"/>
   <link rel="stylesheet" href="base.css" type="text/css">
   <title>Cinelerra :: a video editor and compositor for Linux</title>
 </head>
