d69e39313786619188206b711ee4785978541bc3
Danny Schmarsel <danny@schmarsel.de>
2015-09-08 09:36:42 +0200

    Fancy download page
    Removed Ark Linux, it's gone.
    Built fancy download buttons with scriptaculous.

--- getting_cinelerra.php
+++ getting_cinelerra.php
@@ -31,114 +31,157 @@ You just need to download the .iso file, burn it on a DVD, insert it in the driv
 
 <h2><a name="packages">Packages</a></h2>
 
-<h3>
-<a href="#arch">Arch Linux</a> |
-<a href="#ark">Ark Linux</a> |
-<a href="#apt">Debian</a> |
-<a href="#fedora">Fedora</a> |
-<a href="#gentoo">Gentoo</a> |
-<a href="#mandriva">OpenMandriva</a> |
-<a href="#slack">Slackware</a> |
-<a href="#suse">Suse</a> |
-<a href="#ubuntu">Ubuntu</a> 
-</h3>
-
-<h3><a name="arch">Arch Linux</a></h3>
-
-<blockquote>
-<p> 
-Cinelerra CV is included in the Arch Linux community repository. To install 
+<h4 class="download">Select Your Operating System</h4>
+<div id="distros">
+  <div id="distro-archlinux" class="button">
+    <img src="/images/website/distro-logos/arch.png" alt="ArchLinux Logo" title="Download Cinelerra-CV for ArchLinux" />
+    <h4>Arch Linux</h4>
+    <div class="details">
+      <p> 
+Cinelerra-CV is included in the Arch Linux community repository. To install 
 the "cinelerra" package enable the community repository first (See the <a
 href='http://wiki.archlinux.org/index.php/AUR_User_Guidelines'>User
 Guidelines</a> for more info), then run the following command from the command line: <BR>
 <code>pacman -Sy cinelerra-cv</code> 
-</p>
-</blockquote>
+      </p>
+    </div>
+  </div>
 
-<h3><a name="ark">Ark Linux</a></h3>
+  <div id="distro-debian" class="button">
+    <img src="/images/website/distro-logos/debian.png" alt="Debian Logo" title="Download Cinelerra-CV for Debian" />
+    <h4>Debian</h4>
+    <div class="details">
+      <p> 
+For Debian Stable (Jessie) both CinelerraCV 2.2 and CinelerraHV 4.5 packages are available from <a href="http://deb-multimedia.org/">Deb-multimedia</a>.
+      </p>
+    </div>
+  </div>
 
-<blockquote>
-<p> 
-CinelerraCV is included in the Ark Linux package repository. Install 
-the "cinelerra" package using the "Install Software" tool in Mission Control, 
-or run the following commands from a command line: <BR>
-<code>apt-get update <BR>
-apt-get install cinelerra</code> .
-</p>
-</blockquote>
-
-  <h3><a name="apt">Debian</a></h3>
-
-<blockquote>
-<p>For Debian Stable (Jessie) both CinelerraCV 2.2 and CinelerraHV 4.5 packages are available from <a href="http://deb-multimedia.org/">Deb-multimedia</a>.
-</blockquote>
-
-<h3><a name="gentoo">Gentoo</a></h3>
-
-<blockquote>
-<p>Installation for Gentoo GNU/Linux is very straight forward. Simply type: <BR>
-<code>emerge cinelerra</code><BR>
-as root and it should install and run without any problems. <BR>
-Note that you may need to put cinelerra in your `/etc/portage/package.keywords'
-file in order to unmask it:<BR>
-<code>echo "media-video/cinelerra ~x86" >> /etc/portage/package.keywords </code><BR>
-See the <a href='http://www.gentoo.org/doc/en/handbook/handbook-x86.xml?part=3&chap=3'>Handbook</a> 
-for details. <BR>
-More informations can be found in the <a href="cinelerra-cv.org/docs/cinelerra_cv_manual_en_2.html">Manual</a>.</p>
-</blockquote>
-
-<h3><a name="fedora">Fedora</a></h3>
-
-<blockquote>
-<p>Cinelerra is not available in the default Fedora repositories, nor in the RPM Fusion and Livna repositories.<br>
-The <a href="http://packages.atrpms.net/name/cinelerra/">ATrpms repository</a> offers Cinelerra packages 
-<p>
-If you want to compile CinelerraCV from source you can find detailed instructions on Crazed Mule's blog:</p>
+  <div id="distro-fedora" class="button">
+    <img src="/images/website/distro-logos/fedora.png" alt="Fedora Logo" title="Download Cinelerra-CV for Fedora" />
+    <h4>Fedora</h4>
+    <div class="details">
+      <p> 
+<p>Cinelerra is not available in the default Fedora repositories, nor in the RPM Fusion and Livna repositories.</p>
+The <a href="http://packages.atrpms.net/name/cinelerra/">ATrpms repository</a> offers Cinelerra packages.
+<p>If you want to compile CinelerraCV from source you can find detailed instructions on Crazed Mule's blog:</p>
 <ul>
 <li><a href="http://crazedmuleproductions.blogspot.com/2011/05/fedora-15-cinelerra-install-works.html">for Fedora 15, x86-64</a></li>
 <li><a href="http://crazedmuleproductions.blogspot.com/2010/02/fedora-12-x86-64-cinelerra-install.html">for Fedora 12, x86-64</a></li>
 </ul>
-</blockquote>
+      </p>
+    </div>
+  </div>
+  
+  <div id="distro-gentoo" class="button">
+    <img src="/images/website/distro-logos/gentoo.png" alt="Gentoo Logo" title="Download Cinelerra-CV for Gentoo" />
+    <h4>Gentoo</h4>
+    <div class="details">
+      <p> 
+<p>Installation for Gentoo GNU/Linux is very straight forward. Simply type:</p>
+<p><code>emerge cinelerra</code></p>
+<p>as root and it should install and run without any problems.</p>
+<p>Note that you may need to put cinelerra in your <i>/etc/portage/package.keywords</i>
+file in order to unmask it:</p>
+<p><code>echo "media-video/cinelerra ~x86" >> /etc/portage/package.keywords </code></p>
+<p>See the <a href='http://www.gentoo.org/doc/en/handbook/handbook-x86.xml?part=3&chap=3'>Handbook</a> 
+for details.</p>
+<p>More informations can be found in the <a href="cinelerra-cv.org/docs/cinelerra_cv_manual_en_2.html">Manual</a>.</p>
+      </p>
+    </div>
+  </div>
 
-<h3><a name="mandriva">OpenMandriva</a></h3>
-<p>
-<blockquote>
+  <div id="distro-openmandriva" class="button">
+    <img src="/images/website/distro-logos/openmandriva.png" alt="OpenMandriva Logo" title="Download Cinelerra-CV for OpenMandriva" />
+    <h4>Mandriva</h4>
+    <div class="details">
+      <p> 
 CinelerraCV 2.2 is officially available in OpenMandriva 2014.1.
-</blockquote>
-</p>
+      </p>
+    </div>
+  </div>
 
-<h3><a name="slack">Slackware</a></h3>
-<blockquote>
+  <div id="distro-opensuse" class="button">
+    <img src="/images/website/distro-logos/opensuse.png" alt="OpenSUSE Logo" title="Download Cinelerra-CV for OpenSUSE" />
+    <h4>openSUSE</h4>
+    <div class="details"> 
+<p>RPMs for openSUSE are in the <a href="http://packman.links2linux.org/package/cinelerra">Packman</a> repositories.</p>
+<p>You can install directly with Yast if you have Packman configured as a Yast install source.</p>
+    </div>
+  </div>
+
+  <div id="distro-slackware" class="button">
+    <img src="/images/website/distro-logos/slackware.png" alt="Slackware Logo" title="Download Cinelerra-CV for Slackware" />
+    <h4>Slackware</h4>
+    <div class="details">
+      <p> 
 You can find prebuilt CinelerraCV packages in the <em>multimedia</em> section of the <a href="http://repository.slacky.eu/">Slacky.eu</a> repository.<br>
 If you want to build CinelerraCV from scratch, SlackBuilds are available <a href="http://slackbuilds.org/repository/14.1/multimedia/cinelerra/">at the usual location</a>.
-</blockquote>
+      </p>
+    </div>
+  </div>
 
-<h3><a name="suse">Suse</a></h3>
-
-<blockquote>
-<p>RPMs for openSUSE are in the <a href="http://packman.links2linux.org/package/cinelerra">Packman</a> repositories.<br>
-You can install directly with Yast if you have Packman configured as a Yast install source.</p>
-</blockquote>
-
-<h3><a name="ubuntu">Ubuntu</a></h3>
-
-<blockquote>
+  <div id="distro-ubuntu" class="button">
+    <img src="/images/website/distro-logos/ubuntu.png" alt="Ubuntu Logo" title="Download Cinelerra-CV for Ubuntu" />
+    <h4>Ubuntu</h4>
+    <div class="details">
 <p>Latest CinelerraCV 2.3 for Ubuntu (from 12.04 Precise Pangolin to the current beta release) is available from a Launchpad repo maintained by Nicola Ferralis:</p>
-<blockquote>
 <code>https://launchpad.net/~cinelerra-ppa</code>
-</blockquote>
 <p>To add the Cinelerra PPA to your source list follow the instructions on the <a href="https://launchpad.net/~cinelerra-ppa/+archive/ppa">Launchpad page</a>.</p>
 <p>Once the the PPA has been enabled you can install the package with the command: </p>
-<blockquote>
 <code>sudo apt-get install cinelerra-cv</code>
-</blockquote>
+<br/>
+<p>If you want to compile CinelerraCV from source you can find detailed instructions on the <a href="http://www.g-raffa.eu/Cinelerra/HOWTO/compilation.html">Cinelerra for Grandma</a> tutorial.</p>
+    </div>
+  </div>
+  
+</div>
 
-<p> If you want to compile CinelerraCV from source you can find detailed instructions on the <a href="http://www.g-raffa.eu/Cinelerra/HOWTO/compilation.html">Cinelerra for Grandma</a> tutorial.</p>
-</blockquote>
+<div class="clear"></div>
 
- </div>
+<h3 id="distro-details-header" class="download"></h3>
+
+<div id="distro-details">
+</div>
+
+<!-- async js -->
+<script type="text/javascript">
+
+var download_action = "Install on";
+
+function show_distro_details (button)
+{
+    var title = $$('#' + button.id + ' h4')[0].innerHTML;
+    var content = $$('#' + button.id + ' div.details')[0].innerHTML;
+    
+    button.addClassName ('chosen');
+    $$('#distros div.button').each (function (element, index) {
+        if (element != button) {
+            element.removeClassName ('chosen');
+        }
+    });
+    
+    $('distro-details').innerHTML = content;
+    $('distro-details-header').innerHTML = download_action + ' ' + title;
+}
+
+function install_distro_button_actions ()
+{
+    $$('#distros div.button').each (function (button, index) {
+        
+        button.observe ('click', function () { show_distro_details (button); } );
+    });
+    
+    $('distro-details').setStyle ("display: block");
+}
+
+Event.observe (window, 'load', install_distro_button_actions);
+</script>
+
+
+</div>
  
-<address> Last modified on Sep 5, 2015 </address>
+<address> Last modified on Sep 8, 2015 </address>
 
 <?php include "footer.php"; ?>
  </body>
