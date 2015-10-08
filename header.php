31be38e34ae21e8d89ef4a13d3c1b8373662d53f
Danny Schmarsel <danny@schmarsel.de>
2015-10-08 17:24:21 +0200

    Get rid of the ugly favicon
    Hello, blue.

--- header.php
+++ header.php
@@ -6,6 +6,8 @@
   <meta property="og:image" content="http://cinelerra-cv.org/cinelerra.png" />
   <meta property="og:site_name" content="Cinelerra-CV.org"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
   <link rel="stylesheet" href="/css/base.css" type="text/css">
   <link rel="stylesheet" href="/css/menu.css" type="text/css">
   <link rel="stylesheet" href="/css/pure-min.css">
