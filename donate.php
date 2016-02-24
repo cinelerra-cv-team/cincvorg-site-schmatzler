a84669ca829d7c9f9fa23f207897db6583954f7e
Danny Schmarsel <danny@schmarsel.de>
2016-02-24 20:20:12 +0100

    Add donation page
    This is not linked on the main page yet, until everyone gives the Go for it.

---
+++ donate.php
@@ -0,0 +1,43 @@
+<?php
+$title="Donation";
+include "header.php";
+?>
+
+<div class="content">
+<div class="header">
+    <h1>Donation</h1>
+    <h2>If you enjoy using Cinelerra, please consider making a donation</h2> 
+</div>
+
+  <p>Cinelerra is developed for free by people from all around the world. But like ice cream and candy, all good things cost money.</p>
+  <p>We need to rent a server for:</p>
+  <ul>
+    <li>hosting this website</li>
+    <li>running our <a href="http://git.cinelerra-cv.org/gitweb">GIT repositories</a> for the developers</li>
+    <li>maintaining our <a href="http://lists.cinelerra-cv.org/pipermail/cinelerra/">mailing list</a></li>
+    <li>and <a href="http://cinelerra-cv.org/repo">providing packages</a> for our upcoming LiveCD project</li>
+  </ul>
+  
+<h2>How?</h2>
+  <p>You can donate Bitcoins to the following address:</p>
+  <p style="text-align:center;font-size:150%;">1AvfR2VodNB969p9T1isWZxKkyD3Y4XgfQ</p>
+  
+  <p>For security reasons, this address will be changed on arbitrary intervals. We will always provide the donation history for all addresses.</p>
+  
+  <ul>
+    <li><a href="https://blockchain.info/address/1AvfR2VodNB969p9T1isWZxKkyD3Y4XgfQ?currency=EUR">Donation history</a></li>
+    <li><a href="https://www.bitcoin.com/buy-bitcoin">How to buy Bitcoin</a></li>
+  </ul>
+  
+<h2>Can I help you without Bitcoin?</h2>
+
+  <p>Yes. If you want to make a donation with a different payment method, please leave a message on our mailing list.</p>
+  
+<p>Thank you for supporting us!</p>
+
+<div class="lastmodified">Last modified on Feb 24, 2016</div>
+</div>
+ 
+<?php include "footer.php"; ?>
+ </body>
+</html>
