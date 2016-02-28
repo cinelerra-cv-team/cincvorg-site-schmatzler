44081e52099ecca312e89c3ba1a7f836a7c990c3
Danny Schmarsel <danny@schmarsel.de>
2016-02-28 16:45:43 +0100

    Make text leading to the obsolete ML smaller
    Doesn't need to take up that much space anymore.

--- contact.php
+++ contact.php
@@ -15,9 +15,7 @@ include "header.php";
   <p>You can send an e-mail to <code>&lt;cinelerra <strong>at</strong> lists.cinelerra-cv.org&gt;</code> and all users subscribed to our mailing list will get it.</p>
   <p>There is a web interface for <a href="http://lists.cinelerra-cv.org/cgi-bin/mailman/listinfo/cinelerra">subscribing</a> and a <a href="http://lists.cinelerra-cv.org/pipermail/cinelerra/">web archive</a> that anybody can browse.</p>
   <p>You are <u>not forced</u> to subscribe, but your e-mail will then be held waiting for moderator approval. The same goes for attachments bigger than 400KB.</p>
-  <br />
-  <p>We also have a <a href="https://lists.skolelinux.org/pipermail/cinelerra/">web archive of the old mailing list</a> which was mostly shut down in March 2014.</p>
-  <p>It is also available on <a href="http://www.mail-archive.com/cinelerra@skolelinux.no/">The Mail Archive</a></li> and <a href="http://dir.gmane.org/gmane.comp.video.cinelerra-cv.general">GMANE</a></p>
+  <p style="font-size:small;">We also have a <a href="https://lists.skolelinux.org/pipermail/cinelerra/">web archive of the old mailing list</a> which was mostly shut down in March 2014. It is also available on <a href="http://www.mail-archive.com/cinelerra@skolelinux.no/">The Mail Archive</a></li> and <a href="http://dir.gmane.org/gmane.comp.video.cinelerra-cv.general">GMANE</a>.</p>
   
 <h2>Internet Relay Chat (IRC)</h2>
   <p>We are on the channel <code>#cinelerra</code> on <strong>Freenode</strong>.</p>
@@ -29,7 +27,7 @@ include "header.php";
   <p>You've found a bug on the website or you have a suggestion for it? Please <a href="mailto:danny@schmarsel.de">send me a message!</a></p>
   <p><b>Note:</b> If you'd like to make non-trivial changes or change extensive stuff, please contact the mailing list first for approval.</p>
 
-<div class="lastmodified">Last modified on Sep 5, 2015</div>
+<div class="lastmodified">Last modified on Feb 28, 2016</div>
 </div>
  
 <?php include "footer.php"; ?>
